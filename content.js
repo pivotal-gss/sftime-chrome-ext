aca0ce6949ef402a2dbf747d0a07661fcf101569
Ignacio Elizaga <ielizaga@pivotal.io>
2018-09-26 14:08:36 +0200

    Pause stopwatch functionality
    The plugin will now poll on page load until the stopwatch is
    loaded and it will then pause it, so that wrong time is not logged
    by mistake when closing or refreshing a Salesforce tab.
    
    This is a client workaround and could be permanently fixed by
    diabling the stopwatch in CaseTimer.js.
    
    Authored-by: Ignacio Elizaga <ielizaga@pivotal.io>

---
+++ content.js
@@ -0,0 +1,21 @@
+/**
+ * here we will inject javascript to automatically trigger the time
+ * logging prompt on ticket update and also disable the automatic 
+ * session time stopwatch from running
+ */ 
+
+window.addEventListener ("load", main, false);
+
+function main (evt) {
+    var stopwatchTimer = setInterval (disableStopwatch, 500);
+
+    function disableStopwatch () {
+        console.debug("sftime:: checking if session timer stopwatch is present")
+        // disable automatic time tracking as soon as it is loaded
+        if (document.querySelector ("#stop")) {
+            clearInterval (stopwatchTimer);
+            document.getElementById("stop").click();
+            console.log("sftime:: stopwatch paused")
+        }
+    }
+}
