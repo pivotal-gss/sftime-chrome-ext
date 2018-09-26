88a4afcf4e7edb5b60acd994054423ff3fd6f5ff
Ignacio Elizaga <ielizaga@pivotal.io>
2018-09-26 14:29:08 +0200

    Keep polling for stopwatch
    Kept polling in intervals of one second. If we disable the interval
    the stopwatch won't be paused in new salesforce tabs or on ticket
    reload.
    
    Authored-by: Ignacio Elizaga <ielizaga@pivotal.io>

--- content.js
+++ content.js
@@ -7,13 +7,13 @@
 window.addEventListener ("load", main, false);
 
 function main (evt) {
-    var stopwatchTimer = setInterval (disableStopwatch, 500);
+    var stopwatchTimer = setInterval (disableStopwatch, 1000);
 
     function disableStopwatch () {
         console.debug("sftime:: checking if session timer stopwatch is present")
         // disable automatic time tracking as soon as it is loaded
-        if (document.querySelector ("#stop")) {
-            clearInterval (stopwatchTimer);
+        if (document.querySelector ("#stop")) {  
+            // clearInterval (stopwatchTimer);
             document.getElementById("stop").click();
             console.log("sftime:: stopwatch paused")
         }
